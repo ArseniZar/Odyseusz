d051f8d4147d52e1fb8978f80b9f023c1f1ea75e
ArseniZar <arseni.zarkiewicz@gmail.com>
2025-11-15 00:26:30 +0100

    chore(4-create-pages): update config files, add tailwind-scrollbar support, pin Tailwind CSS version

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import tailwind_scrollbar from 'tailwind-scrollbar'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(tailwind_scrollbar)],
 })
