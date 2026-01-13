f3f2625fab872b545a94f05f8b8cad95d46c9544
ArseniZar <arseni.zarkiewicz@gmail.com>
2026-01-14 00:31:08 +0100

    feat(4-create-pages): enhance Input and add Select component
    - Updated Input component and its types
    - Added new Select component with types and index export

--- client/src/components/Input/Input.tsx
+++ client/src/components/Input/Input.tsx
@@ -1,8 +1,10 @@
-import type { JSX } from "react";
+import {type JSX } from "react";
 import type { InputProps } from "./Input.types";
-
+import { IconButton } from "@/components/IconButton";
+import {iconCross} from "@/assets/"
 //prettier-ignore
 export const Input = ({type = "text", label, placeholder ="", value, tooltipText, className ="", classInput ="", ref = null, onChange, onFocus, onBlur}: InputProps): JSX.Element => {
+  const stringValue = value == null ? "" : String(value);
   return (
     <div className={`relative flex flex-col gap-2 group ${className}`}>
       <label className="ml-2 text-lg tracking-wide">{label}</label>
@@ -11,16 +13,24 @@ export const Input = ({type = "text", label, placeholder ="", value, tooltipText
           <p className="break-normal">{tooltipText}</p>
         </div>
       )}
+      <div className={`flex flex-row items-center pr-2 rounded-2xl border-2 border-black/10 shadow-xl focus-within:border-blue-600 ${classInput}`}>
       <input
-        className={`py-2 px-3 rounded-2xl border-2 border-black/10 shadow-xl ${classInput}`}
+        className={` min-w-0 flex-1 py-3 px-4 outline-none`}
         placeholder={placeholder}
-        value={value}
+        value={stringValue}
         type={type}
         ref={ref} 
-        onChange={(e) => onChange && onChange(e.target.value)}
-        onFocus={(e) => onFocus && onFocus(e)}
-        onBlur={(e) => onBlur && onBlur(e)}
+        onChange={e => {onChange?.(e.target.value)}}
+        onFocus={(e) => onFocus?.(e)}
+        onBlur={(e) => onBlur?.(e)}
       />
+       <IconButton
+          icon={iconCross}
+          classIcon="w-5 h-5 p-0.5"
+          classButton={`p-1 rounded-full  transition-opacity  ${stringValue.length > 0 ? "visible" : "invisible"}`}
+          onClick={() => onChange?.("")}
+        />
+      </div>
     </div>
   );
 };
