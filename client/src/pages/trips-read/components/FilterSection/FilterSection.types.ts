f454a9968b9d431d42ff0624f38ae7419d8c73f1
ArseniZar <arseni.zarkiewicz@gmail.com>
2026-01-28 00:36:03 +0100

    refactor(4-create-pages): update trips read page and filter section

--- client/src/pages/trips-read/components/FilterSection/FilterSection.types.ts
+++ client/src/pages/trips-read/components/FilterSection/FilterSection.types.ts
@@ -1,10 +1,9 @@
-import type { Control, FieldErrors } from "react-hook-form";
+import type { Control } from "react-hook-form";
 import type { FilterValue } from "../../TripsReadPage.types";
 import type { FilterSectionConfig } from "../../config/filterSection.config.types";
 
 export interface FilterSectionProps {
   infoText: FilterSectionConfig
   control: Control<FilterValue>;
-  errors?: FieldErrors<FilterValue>;
   onCreate: () => void;
 }
