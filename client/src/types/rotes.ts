c7164c1b615d5fbfd17851f9b33e55976683dd3b
ArseniZar <arseni.zarkiewicz@gmail.com>
2026-01-28 14:23:52 +0100

    feat(4-create-pages): add icon assets, update App component, and adjust types

--- client/src/types/rotes.ts
+++ client/src/types/rotes.ts
@@ -2,11 +2,15 @@ export type AppRoutes =
   | "TRIP_CREATE"
   | "TRIPS_READ"
   | "TRIP_EDIT"
-  | "EVACUATION_CREATE";
+  | "EVACUATION_CREATE"
+  | "EVACUATIONS_READ"
+  | "EVACUATION_EDIT" 
 
 export const routesConfig: Record<AppRoutes, {path:string, label:string}> = {
   TRIP_CREATE: {path:"/trips/create", label:"Create Trip"},
   TRIPS_READ: {path:"/trips/read", label:"Read Trips"},
   TRIP_EDIT: {path:"/trips/edit/:tripId", label:"Edit Trip"},
   EVACUATION_CREATE: {path:"/evacuations/create", label:"Create Evacuation"},
+  EVACUATIONS_READ: {path:"/evacuations/read", label:"Read Evacuations"},
+  EVACUATION_EDIT: {path:"/evacuations/edit/:evacuationId", label:"Edit Evacuation"},
 };
